412f8e42074aa263770a159f3ea8c6275075ea20
Javad Fadaieghotbi <qotbi.javad@gmail.com>
2025-07-20 12:19:43 +0200

    Adding the new message

---
+++ include/driver_registery.hpp
@@ -0,0 +1,27 @@
+#ifndef DRIVER_REGISTERY_HPP
+#define DRIVER_REGISTERY_HPP
+
+#include <string>
+
+class driver_registery{
+
+    public : 
+
+        driver_registery
+            ( int _driverId
+            , std::string _driver_name
+            , std::string _manufacturer_name)
+            : driverId(_driverId)
+            , driver_name(_driver_name)
+            , manufacturer_name(_manufacturer_name)
+            {}
+
+
+    protected : 
+    
+        int driverId;
+        std::string driver_name;
+        std::string manufacturer_name;
+};
+
+#endif
